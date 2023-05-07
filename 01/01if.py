ec28f79c1e503a2aef43e0db53f8cbdaa6f0776c
genie-ru <genie.zgmf987@gmail.com>
2023-05-08 00:01:23 +0900

    first

---
+++ 01/01if.py
@@ -0,0 +1,19 @@
+x = 101
+if x == 10:
+    print(10)
+    exit()
+else:
+    print("This isn't 10")
+    
+a = 1
+b = 1
+"""
+a == b
+a != b
+a < b
+a > b
+a <= b
+a => b
+a > 0 and b > 0
+a > 0 or b > 0
+"""
---
+++ 01/02in_not.py
@@ -0,0 +1,14 @@
+y = [1, 2, 3]
+x = 1
+
+if x in y:
+    print('in')
+if 100 not in y:
+    print('not in')
+a = 1
+b = 2
+
+if not a == b:
+    print('Not equal')
+if  a == b:
+    print('Not equal')
